7febae0ce7bdb45bec85c10ead6822b0d915cb35
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-03-01 22:23:58 +0600

    server setup done

--- server-exam/index.js
+++ server-exam/index.js
@@ -8,11 +8,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Setup is ok")
-})
 
-//crud operation's code
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i0wokhn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        app.get("/", (req, res) => {
+            res.send("Setup is ok")
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.listen(port, () => {
     console.log(`the app is running on port ${port}`);
